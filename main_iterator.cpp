de5ed3561f055777528a442b03fa382a8b8573b2
Anthony <zamora.anthony82913@gmail.com>
2019-02-09 15:04:54 -0500

    Add files via upload

---
+++ main_iterator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "NodeList.cpp"
+
+
+using namespace std;
+int main(int argc, char** argv)
+{
+	NodeList<int> L;
+ 
+	for (int i = 0; i < 10; ++i)
+	{
+		NodeList<int>::Iterator ii = L.begin();
+		int value;
+        cout << "Enter a value: ";
+		cin >> value;
+		L.insert(ii, value);
+		++ii;
+	}
+	cout << "Printing the values you just entered " << endl;
+	for (NodeList<int>::Iterator ii=L.begin(); ii!=L.end(); ++ii)
+	{
+		cout << *ii << endl;
+	}
+    
+	cout << "\nNow printing the values backwards " << endl;
+	for (NodeList<int>::Iterator ii=L.end(); ii!=L.begin(); --ii)
+	{
+        // FYI: Once "ii" reaches the beginning of ADT, the loop stops
+        
+		if (ii == L.end()) continue;
+		cout << *ii << endl;
+	}
+	return 0;
+}
